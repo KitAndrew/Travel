9071b2456a6c281810fa7319a4894aeeb877d572
KitAndrew <im.leexiao@gmail.com>
2019-05-25 18:36:17 +0800

    project city header

---
+++ src/pages/city/City.vue
@@ -0,0 +1,12 @@
+<template>
+  <city-header></city-header>
+</template>
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {CityHeader},
+  CityHeader
+}
+</script>
+<style lang="stylus" scoped></style>
