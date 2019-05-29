662ea15781cfbf4d3035f9a4523a53ace0478e9d
KitAndrew <im.leexiao@gmail.com>
2019-05-29 22:59:39 +0800

    project  detail 递归组件实现详情页列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,32 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
-  components: {DetailHeader, DetailBanner},
+  components: {DetailList, DetailHeader, DetailBanner},
   comments: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {title: '成人票', children: [{title: '成人三馆联票', children: [{title: '成人三馆联票-1'}]}]},
+        {title: '儿童票'},
+        {title: '学生票'},
+        {title: '特惠票'}
+      ]
+    }
   }
 }
 </script>
